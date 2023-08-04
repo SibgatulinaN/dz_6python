165bfc403fb4f19e5c0ca5d2536bde321f63ef8a
Наталья Сибгатулина <mehryak@mail.ru>
2023-08-04 19:35:18 +0500

    Добавлено еще решение 30 задачи

--- task30.py
+++ task30.py
@@ -3,9 +3,9 @@
 # элементов нужно ввести с клавиатуры. Формула для
 # получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
-n = int(input('Введите первый элемент: '))
-s = int(input('Введите разность : '))
-q = int(input('Введите количесто элементов: '))
+# n = int(input('Введите первый элемент: '))
+# s = int(input('Введите разность : '))
+# q = int(input('Введите количесто элементов: '))
 
 
 def arithmetic_progression(n, s, q):
@@ -14,3 +14,9 @@ def arithmetic_progression(n, s, q):
         arr.append(arr[i-1] + s)
     return arr
 print(arithmetic_progression(n, s, q))    
+
+a1 = 3
+d = 2
+n = 10
+for i in range(n):
+    print(a1 + i * d, end=' ')
